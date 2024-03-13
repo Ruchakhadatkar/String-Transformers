ea794c3fdfc557f24d55d3efb8cb77413297b675
Rucha Khadatkar <ruchakhadatkar83@gmail.com>
2024-03-13 16:49:41 +0530

    Added string transform function for trimcase

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ const camelCaseOutput = document.querySelector("#camelcase span");
 const pascalCaseOutput = document.querySelector("#pascalcase span");
 const snakeCaseOutput = document.querySelector("#snakecase span");
 const kebabCaseOutput = document.querySelector("#kebabcase span");
-const trimOutput = document.querySelector("#trim span");
-
-
+const trimOutput = document.querySelector("#trimcase span");
 
 function capitalizeString(str) {
   return str[0].toUpperCase() + str.slice(1, str.length);
@@ -25,27 +23,37 @@ function camelCase(str) {
 }
 
 function pascalCase(str) {
-    const lowerCaseString = str.toLowerCase();
-    const wordArray = lowerCaseString.split(" ");
-    const finalArray = wordArray.map((word, i) => {
-      return capitalizeString(word);
-    });
-    return finalArray.join("");
-  }
+  const lowerCaseString = str.toLowerCase();
+  const wordArray = lowerCaseString.split(" ");
+  const finalArray = wordArray.map((word, i) => {
+    return capitalizeString(word);
+  });
+  return finalArray.join("");
+}
 
-  function snakeCase(str) {
-    const wordArray = str.split(" ");
-    
-    return wordArray.join("_");
-  }
+function snakeCase(str) {
+  const wordArray = str.split(" ");
+  return wordArray.join("_");
+}
+
+function kebabCase(str) {
+  const wordArray = str.split(" ");
+  return wordArray.join("-");
+}
+
+function trimCase(str) {
+  const wordArray = str.split(" ");
+  return wordArray.join("");
+}
 
 function updateScreen() {
   lowerCaseOputput.innerText = input.value.toLowerCase();
   upperCaseOputput.innerText = input.value.toUpperCase();
   camelCaseOutput.innerText = camelCase(input.value);
-  pascalCaseOutput.innerText = pascalCase(input.value)
-  snakeCaseOutput.innerText = snakeCase(input.value)
-
+  pascalCaseOutput.innerText = pascalCase(input.value);
+  snakeCaseOutput.innerText = snakeCase(input.value);
+  kebabCaseOutput.innerText = kebabCase(input.value);
+  trimOutput.innerText = trimCase(input.value);
 }
 
 updateScreen();
